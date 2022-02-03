f3cdb397b1e675cfcde40e1da2bc6a34eff86282
Дмитрий Тютюнник <dmytrotiuk@gmail.com>
2022-02-03 18:02:29 +0200

    Finish project

---
+++ accounts/models.py
@@ -0,0 +1,61 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    """Manager for user profiles."""
+
+    def create_user(self, email, username, password=None):
+        """Creates and saves a User with the given email, date of birth and password."""
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        """Creates and saves a superuser with the given email, date of birth and password."""
+        user = self.create_user(
+            email,
+            password=password,
+            username=username,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    username = models.CharField(_("username"), max_length=150, blank=True, null=True)
+    email = models.EmailField(_("email address"), unique=True)
+    created_at = models.DateTimeField(_("created at"), default=timezone.now)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+    dob = models.DateField(_("date of birth"), blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        """String representation of User in admin."""
+        return self.email
+
+    def get_full_name(self):
+        """Return the first_name plus the last_name, with a space in between."""
+        return f"{self.first_name} {self.last_name}".strip()
+
+    def get_age(self):
+        if self.dob is None:
+            return None
+        return (timezone.now() - self.dob).years
