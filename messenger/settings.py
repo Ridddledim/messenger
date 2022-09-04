b0d01357942eb5429dab9ab60de2a77290d7fd24
Дмитрий Тютюнник <dmytrotiuk@gmail.com>
2022-09-05 01:39:09 +0300

    add some fixes

--- messenger/settings.py
+++ messenger/settings.py
@@ -29,16 +29,6 @@ INSTALLED_APPS = [
     "apps.dialogs",
 ]
 
-DATABASES = {
-    "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "HOST": os.environ.get("DB_HOST"),
-            "NAME": os.environ.get("DB_NAME"),
-            "USER": os.environ.get("DB_USER"),
-            "PASSWORD": os.environ.get("DB_PASS"),
-        },
-}
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
